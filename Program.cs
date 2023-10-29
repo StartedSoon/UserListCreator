4547237236af9e019331ae8a25b1335873952ce4
HP <HP@DESKTOP-UCDQ0S1>
2023-10-29 17:29:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Kaç kişilik bir liste oluşturmak istiyorsunuz ?"); //yazı
+int maxUser = Math.Clamp(Convert.ToInt32(Console.ReadLine()), 0,1000000); //en fazla kaç kişi olacağını tanımlıyoruz tanım
+Console.WriteLine($"{maxUser} kişilik bir liste oluşturuldu"); //yazı grup oluşturuldu
+string[] UserList = new string[maxUser]; //tanım-1 liste oluşturuldu
+int userID; //kullanıcının sayısının kimliği tanımlandı.
+for (userID = 0; userID <= maxUser - 1; userID++)
+{
+    Console.WriteLine("Listeye eklenecek bir kullanıcı adı giriniz. Eğer durmak istiyorsanız dur yazınız. ");
+    string yazi = Console.ReadLine();
+    if (yazi == "dur")
+    {
+        Console.WriteLine("Liste tamamlandı.");
+        break;
+    }
+    else
+    {
+        UserList[userID] = yazi;
+    }
+}
+Console.WriteLine("Liste Sıralanıyor...");
+for (int sira = 0; sira <= userID - 1; sira++)
+{
+    Console.WriteLine(UserList[sira]);
+}
+
